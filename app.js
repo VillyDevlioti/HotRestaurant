3cc66e5d06005c54bf7200335564537cbb6a86d9
k-lord <katielord27@gmail.com>
2019-12-11 20:29:08 -0500

    updated name of router in default express files from user to api

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/api', apiRouter);
 
 module.exports = app;
+
+//
