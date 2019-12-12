0e1440b40241a0cfca84e842630c86b43ecc79fd
Villy Devlioti <vadevlio@gmail.com>
2019-12-11 21:23:18 -0500

    fixed the export stuff

--- data.js
+++ data.js
@@ -2,4 +2,10 @@ const tables = [];
 const waitlist =[];
 var reservations=[];
 
+var data = {
+    tables: tables,
+    waitlist: waitlist,
+    reservations: reservations
+};
+
 module.exports = data;
